ee738cc08798affe07cfac8bfe4276a679e09946
carlosordo <94583318+carlosordo@users.noreply.github.com>
2025-02-13 02:49:57 -0500

    Add files via upload
    Se retira libreria que no se usa

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
-import matplotlib.pyplot as plt
 
 # Configuración de la aplicación
 st.set_page_config(page_title="My Valentine", layout="wide")
